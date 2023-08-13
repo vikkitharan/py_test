2d98f7d44db311e8b55c0d2495798d5d38c9f22a
Vikki <vikki.gsam@gmail.com>
2023-08-14 06:09:53 +1000

    Add files

---
+++ test_shoping_cart.py
@@ -0,0 +1,31 @@
+from shoping_cart import ShoppingCart
+import pytest
+
+def test_can_add_item_to_cart():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert cart.size() == 1
+
+
+def test_when_item_added_then_cart_contains_item():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    assert "apple" in  cart.get_items()
+
+def test_when_add_more_than_max_items_should_fail():
+    cart = ShoppingCart(5)
+    for _ in range(5):
+        cart.add("apple")
+
+    with pytest.raises(OverflowError):
+        cart.add("apple")
+
+def test_can_get_total_price():
+    cart = ShoppingCart(5)
+    cart.add("apple")
+    cart.add("orange")
+    price_map = {
+            "apple" : 1.0,
+            "orange" : 2.0}
+
+    assert cart.get_total_price(price_map) == 3.0
